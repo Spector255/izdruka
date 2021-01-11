b5866dfe08cc363371db75de6380f9e9404cbdc7
Daniils Zemīts <daniks255@inbox.lv>
2021-01-11 12:49:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hello, world!")
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+a = 1 + 2
+print(a)
+b = "a" + "b" + "c"
+print(b)
+c = 'a' * 2
+print(c)  #2a
+'''Comment'''
+print(1000000 + 25)  #English + Latvian
+print(15 / 4)
+print(17 // 4)
+print(17 % 4)
+print(f"5+7 būs {5+7}.")
+'''Com'''
+print("1" + "2")
+"""Ļoti liels
+teksts
+Nu Ļoti
+"""
+print ("------------------")
+
+print("Daniils "*5)
+print(f"2021 gadā ir {60*60*24*365} sekundes")
+print (1/2)
+print()
