38ba27bda49ab85469d748e02d00132a2449af6f
Daniils Zemīts <daniks255@inbox.lv>
2021-01-15 12:02:36 +0000

    Update, sorting

---
+++ izdruka.py
@@ -0,0 +1,56 @@
+print("Hello, world!")
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+summa = 1 + 2
+print(summa)
+b = "a" + "b" + "c"
+print(b)
+c = 'a' * 2
+print(c)  #2a
+'''Comment'''
+print(1000000 + 25)  #English + Latvian
+print(15 / 4)
+print(17 // 4)
+print(17 % 4)
+print(f"5+7 būs {5+7}.")
+'''Com'''
+print("1" + "2")
+"""Ļoti liels
+teksts
+Nu Ļoti
+"""
+print("------------------")
+
+print("Daniils " * 5)
+print(f"2021 gadā ir {60*60*24*365} sekundes")
+print(1 / 2)
+print(2)
+
+print("------------------")
+#Mainigie
+saraksts = ["rex", "bonux"]
+print(saraksts)
+print(type(saraksts))
+saraksts = 2
+print(saraksts)
+print(type(saraksts))
+#-----------------------------
+#Uzdevumi:
+a = 5
+print(a)
+print(a + a)
+a = a + a
+print(a)
+print(type(a))
+a = 30.2
+print(type(a))
+print()
+#---------------------Nodokļu uzdevums
+mani_ienakumi = 367
+nodoklis = 0.1  #10%
+mani_nodokli = mani_ienakumi * nodoklis
+print(f"Jums jāsamaksa {mani_nodokli}€ apmerā nodokļus")
+print()
+#---------------------
+#datu ievade
+vards = input("Ievadi vārdu: ")
+print(f"Sveiki, " + vards)
